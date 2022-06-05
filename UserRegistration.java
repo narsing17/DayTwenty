c8045dfe701922e059290b3371be0e61a827eaaf
narsing17 <narsingp14@gmail.com>
2022-06-05 22:53:42 +0530

    AddedValidMobileNumber

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,6 +3,8 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+    private static final String PHONE_PATTERN = "^[9]{1}[1]{1}[ ]?[6-9]{1}[0-9]{9}$";
+
 
 
     public boolean validateFirstName(String fname) {
@@ -18,5 +20,9 @@ public class UserRegistration {
     public boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matches(EMAIL_PATTERN, email);
+    }
+    public boolean validatePhoneNumber(String phNumber) {
+        Pattern pattern = Pattern.compile(PHONE_PATTERN);
+        return pattern.matches(PHONE_PATTERN,phNumber);
     }
 }
