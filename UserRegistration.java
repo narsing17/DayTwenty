e6f92c122900fb6108c4eecf8a023a57a6cf16cd
narsing17 <narsingp14@gmail.com>
2022-06-05 22:43:25 +0530

    AddedEmail

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,6 +2,8 @@ package com.userregistration;
 import java.util.regex.Pattern;
 public class UserRegistration {
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
 
     public boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -11,5 +13,10 @@ public class UserRegistration {
     public boolean validateLastName(String lName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matches(NAME_PATTERN, lName);
+    }
+
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matches(EMAIL_PATTERN, email);
     }
 }
